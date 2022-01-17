140047fb26b3684797dc2cb0f1289ebd3fd4e2cf
congphamtienthanh <congphamtienthanh@gmail.com>
2022-01-17 14:53:08 +0700

    demo header

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Trang chủ</title>
 </head>
-<style>
-</style>
 <body>
+    <header>
+        <h2>Shop mua khóa học chẳng hạn</h2>
+        <form method="get" action="">
+            <div id="search">
+                <i class='search bx bx-search'></i>
+                <input type="text" name="search" placeholder= "Tìm kiếm khóa học mà bạn thích">
+            </div>
+        </form>
+        <div class=user>
+            <a href="./login_and_register.php">Đăng nhập</a>
+        </div>
+    </header>
+    <div class="hinder"></div>
     <ul>
         <li>
             <a href="./login_and_register.php">Đăng nhập</a>
