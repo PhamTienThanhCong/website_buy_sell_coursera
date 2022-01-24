65ad2cabffdac31e11e3d5110c26bde09ffa1b85
congphamtienthanh <congphamtienthanh@gmail.com>
2022-01-24 11:18:47 +0700

    user is done

--- my_account.php
+++ my_account.php
@@ -19,6 +19,34 @@
             }
         }
     }
+
+    $sql = "SELECT
+                course.id_course,
+                course.name_course,
+                course.author,
+                course.image_course,
+                course.price,
+                oder.creat_at,
+                oder.rate,
+                oder.history_lesson
+            FROM
+                course
+            INNER JOIN oder ON oder.id_course = course.id_course
+            WHERE
+                oder.id_user = '$id_user'";
+
+    $all_courses = mysqli_query($connection, $sql);
+
+    $sql = "SELECT
+                COUNT(*) as total_buy,
+                SUM(course.price) as total_price
+            FROM
+                oder
+            INNER JOIN course ON course.id_course = oder.id_course
+            WHERE
+                id_user = '$id_user'";
+    $total = mysqli_query($connection, $sql);
+    $total = mysqli_fetch_array($total);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -80,7 +108,7 @@
     .input-replace{
         border-bottom: 1px solid #d3d9de;
     }
-    .btn{
+    .btn,.btn-rate{
         outline: none;
         border: none;
         padding: 10px 20px;
@@ -89,6 +117,17 @@
         border-radius: 10px;
         transition: all 0.5s;
         cursor: pointer;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    .btn-rate{
+        padding: 5px 10px;
+        background-color: #758bbc;
+    }
+    .btn-rate:hover{
+        background-color: #2697ff;
     }
     .btn:hover{
         transform: scale(0.95);
@@ -103,7 +142,7 @@
     #my-password{
         /* transform: translateY(-170px); */
         /* margin-left: 50px; */
-        width: 500px;
+        width: 490px;
         height: 200px;
         display: inline-block;
         display: none;
@@ -116,11 +155,49 @@
         width: calc(100% - 150px);
         margin-top: 40px;
     }
+    .course{
+        margin-top: 20px;
+    }
+    .img-course{
+        display: inline-block;
+        width: 340px;
+        height: 190px;
+    }
+    .img-course img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+    .in4-course{
+        display: inline-block;
+        width: 500px;
+        margin-left: 15px;
+    }
+    .in4-course p{
+        font-size: 18px;
+        margin-top: 4px;
+    }
+    .rate-vote{
+        margin-top: -5px;
+        transform: translateY(-8px)
+    }
+    p i {
+        transform: translateY(2px);
+        margin-right: 5px;
+    }
+    .your-cart p{
+        font-size: 18px;
+        margin-top: 4px;
+    }
 </style>
 <body>
     <?php require "./default/header.php"; ?>
     <div class=content>
-        <h2 >Tài khoản của tôi </h2>
+        <h2 >
+            Tài khoản của tôi 
+            <i class='bx bx-user'></i>
+        </h2>
         <p class="money-style">Ngân hàng Quân đội (MBBANK)</p>
         <p class="money-style" id = "stk">
             Số tài khoản: 
@@ -165,9 +242,83 @@
             <input class="input-in4 input-replace" type="number" name="money" value="<?php echo $_SESSION['money']?>">
             <button class="btn btn-danger">Lưu Thông tin lại</button>
         </form>
-        <div class = "your-cart">
-            <h2>Giỏ hàng đã mua</h2>
+
+        <div class="your-cart">
+            <h2>
+                Tổng quan về tài khoản của bạn
+                <i class='bx bx-file-find'></i>
+            </h2>
+            <p>
+                <i class='bx bxs-right-arrow-alt'></i>
+                Số khóa học đã mua: <?php echo $total['total_buy']?> khóa
+            </p>
+            <p>
+                <i class='bx bxs-right-arrow-alt'></i>
+                Số tiền đã chi trả: <?php echo currency_format($total['total_price'])?>
+            </p>
+            <p>
+                <i class='bx bxs-right-arrow-alt'></i>
+                Số tiền còn lại: <?php echo currency_format($_SESSION['money'])?>
+            </p>
+            <p>
+                <i class='bx bxs-right-arrow-alt'></i>
+                Số tiền đã nạp thêm: 
+                <?php 
+                    if($_SESSION['money'] > 12000000 - $total['total_price']){
+                        echo currency_format($_SESSION['money'] - (12000000 - $total['total_price']));
+                    }else{
+                        0;
+                    }
+                ?>
+            </p>
         </div>
+        <div class = "your-cart">
+            <h2>
+                Giỏ hàng đã mua
+                <i class='bx bx-cart'></i>
+            </h2>
+            <?php foreach ($all_courses as $course) { ?>
+                <div class = "course">  
+                    <div class="img-course">
+                        <img src="./public/images/upload/<?php echo $course['image_course']?>" alt="">
+                    </div>
+                    <div class = "in4-course">
+                        <a href="./my_course_view_lesson.php?idcourse=<?php echo $course['id_course'] ?>">
+                            <h2>Tên: <?php echo $course['name_course']?></h2>
+                        </a>
+                        <p>
+                            <i class='bx bx-user-check'></i>
+                            Tác Giả: <?php echo $course['author']?>
+                        </p>
+                        <p>
+                            <i class='bx bx-credit-card-front'></i>
+                            Giá Tiền: <?php echo currency_format($course['price'])?>
+                        </p>
+                        <p>
+                            <i class='bx bx-time-five'></i>
+                            Thời gian mua: 
+                            <?php echo date("H:i:s - d/m/Y", strtotime($course['creat_at']))?>
+                        </p>
+                        <p>
+                            <i class='bx bx-book-content'></i>
+                            Đã học: <?php echo $course['history_lesson']?> 
+                            Bài 
+                        </p>
+                        <p>
+                            <i class='bx bx-star'></i>
+                            Xếp hạng: <?php echo $course['rate']?>
+                        </p>
+                        <br>
+                        <p class="rate-vote">
+                            <a class="btn-rate" href="./view_course.php?id=<?php echo $course['id_course'] ?>">
+                                Đánh giá hoặc đánh giá lại
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+        
     </div>
     <div class="tab-right"></div>
     </div>
