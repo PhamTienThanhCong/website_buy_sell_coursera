1b1f259672b3afa6af86ff1f89fe7ac6d4802f61
congphamtienthanh <congphamtienthanh@gmail.com>
2022-01-27 08:32:34 +0700

    banner tet

--- default/slide.php
+++ default/slide.php
@@ -1,3 +1,15 @@
+<style>
+    .slide{
+        overflow: hidden;
+        background-color: #a80808;
+    }
+    .slide img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
 <div class="slide">
-    
+    <img src="./public/images/default/banner_tet_2022.jpg" alt="">
 </div>
