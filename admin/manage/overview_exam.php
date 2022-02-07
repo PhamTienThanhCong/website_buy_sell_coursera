2d1603a875c0c2735d57fd19a9c982f8e730b4d2
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-02-07 22:30:23 +0700

    update tổng quan

--- admin/manage/overview_exam.php
+++ admin/manage/overview_exam.php
@@ -12,7 +12,7 @@ require "../../public/connect_sql.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>example</title>
+    <title>Tổng quan</title>
     <!-- Css default -->
     <link rel="stylesheet" href="../default/style.css">
     <link href='https://unpkg.com/boxicons@2.0.7/cs0s/boxicons.min.css' rel='stylesheet'>
@@ -22,6 +22,18 @@ require "../../public/connect_sql.php";
     /* Phần in đậm của option */
     #course-overview {
         background: #081D45;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td, th {
+        border: 1px solid #dddddd;
+        background: azure;
+        text-align: left;
+        padding: 8px;
     }
 
     /* Phần in đậm của option */
@@ -54,45 +66,127 @@ require "../default/option.php"
 
 
                 <p>
-                <?php
+
+                    <?php
                     if ($_SESSION["lever"] == 2) {
+                        print("Quản lý tổng quan admin<br>");
                         $result = mysqli_query($connection, "select count(*) as count from course");
                         print("Số khóa học hiện có: " . mysqli_fetch_array($result)["count"]);
                         print("<br>");
-                    }
-                    $result=mysqli_query($connection,"select count(*) as count from course where id_admin = '{$_SESSION["id"]}'");
-                    print("Số khóa học đang sở hữu: " . mysqli_fetch_array($result)["count"]);
-                    print("<br>");
-                    if ($_SESSION["lever"] == 2) {
                         $result = mysqli_query($connection, "select count(*) as count from course where status_course=0");
                         print("Số khóa học đang chờ phê duyệt: " . mysqli_fetch_array($result)["count"]);
                         print("<br>");
+                        $result = mysqli_query($connection, "select count(*) as count from user");
+                        print("Số học sinh: " . mysqli_fetch_array($result)["count"]);
+                        print("<br>");
+                        $result = mysqli_query($connection, "select count(*) as count from admin");
+                        print("Số giáo viên hiện tại: " . mysqli_fetch_array($result)["count"]);
+                        print("<br>");
+                        $result = mysqli_query($connection, "select count(*) as count from admin");
+                        print("Số giáo viên chờ phê duyệt: " . mysqli_fetch_array($result)["count"]);
+                        print("<br>");
                     }
+                    ?>
+                </p>
+                <p>
+                    Quản lý cho giáo viên<br>
+                    <?php
+                    $result = mysqli_query($connection, "select count(*) as count from course where id_admin = '{$_SESSION["id"]}'");
+                    print("Số khóa học đang sở hữu: " . mysqli_fetch_array($result)["count"]);
+                    print("<br>");
                     $result = mysqli_query($connection, "select count(*) as count from course where status_course=1 and id_admin = '{$_SESSION["id"]}'");
                     print("Số khóa học đã được phê duyệt của bạn: " . mysqli_fetch_array($result)["count"]);
                     print("<br>");
-                ?>
+                    $sql = "select count(*) as count from oder where id_course in (select id_course from course where status_course=1 and id_admin = '{$_SESSION["id"]}')";
+                    $result = mysqli_query($connection, $sql);
+                    print("Số lượt mua của bạn: " . mysqli_fetch_array($result)["count"]);
+                    print("<br>");
+                    ?>
                 </p>
-                <!-- Code here -->
-                <br>
+            </div>
 
-                <br><br>
-                <p>Sửa xong đi cho kẹo =))</p>
-                <!-- Code here -->
+            <div style="height: 300px; " class="recent-sales box">
+                <div class="title">List khóa học</div>
+                <?php
 
+                if (!function_exists('currency_format')) {
+                    function currency_format($number, $suffix = ' VND')
+                    {
+                        if (!empty($number)) {
+                            return number_format($number, 0, ',', '.') . "{$suffix}";
+                        }
+                    }
+                }
+
+                require "../../public/connect_sql.php";
+                $id = $_SESSION['id'];
+                $sql = "SELECT course.*, COUNT(lesson.id_course) as number_course FROM `course` 
+                                LEFT OUTER JOIN lesson ON course.id_course = lesson.id_course
+                                WHERE course.id_admin = '$id' 
+                                GROUP BY course.id_course";
+                $courses = mysqli_query($connection, $sql);
+
+                ?>
+                <div style="height: 90%;overflow-y: scroll;">
+                    <table style="overflow-y: scroll; height: 50vh;">
+                        <thead style="position: sticky; top: 0">
+                        <tr>
+                            <th>STT</th>
+                            <th>Tên khóa học</th>
+                            <th>Giá</th>
+                            <th>Số lượng bài giảng</th>
+                            <th>Trạng thái</th>
+                            <th>Số lượt mua</th>
+                        </tr>
+                        </thead>
+                        <?php foreach ($courses
+
+                        as $index => $course) { ?>
+                        <tr>
+                            <td>
+                                <?php echo $index + 1; ?>
+                            </td>
+                            <td>
+                                <?php echo $course['name_course'] ?>
+                            </td>
+                            <td>
+                                <?php echo currency_format($course['price']) ?>
+                            </td>
+                            <td>
+                                <?php echo $course['number_course'] ?>
+                            </td>
+                            <td>
+                                <?php
+                                if ($course['status_course'] == 0) {
+                                    echo "Chờ xác nhận";
+                                } else {
+                                    echo "Đã xác nhận";
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    $sql = "select count(*) as count from oder where id_course = '{$course['id_course']}'";
+                                    $result = mysqli_query($connection, $sql);
+                                    print(mysqli_fetch_array($result)["count"]);
+                                ?>
+                            </td>
+                            <?php } ?>
+                        <tr>
+                    </table>
+                </div>
             </div>
         </div>
         <!-- đây là 1 khối nội dung -->
-
-        <!-- Phần chân trang -->
-        <?php require "../default/footer.php" ?>
-        <!-- Phần chân trang -->
 
     </div>
     <!-- Phần content chính -->
 
 </section>
 <!-- hàm section chính -->
+<!-- Phần chân trang -->
+<?php require "../default/footer.php" ?>
+<!-- Phần chân trang -->
 </body>
 <!-- javaScript Chung :3 -->
 <script type="text/javascript" src="./script/js_chung.js"></script>
