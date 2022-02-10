61ecf4b454fff6e2bff6bf8bc47d67483a34c966
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-02-10 23:16:05 +0700

    tự chuyển sang trang đăng nhập nếu người dùng chưa đăng nhập khi bấm mua ở giỏ hàng

--- my_cart.php
+++ my_cart.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-if (isset($_SESSION['lever'])){session_destroy();}
+if (isset($_SESSION['lever'])) {
+    session_destroy();
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,93 +18,94 @@ if (isset($_SESSION['lever'])){session_destroy();}
 </head>
 
 <body>
-    <?php 
-        if (!function_exists('currency_format')) {
-            function currency_format($number, $suffix = ' VND')
-            {
-                if (!empty($number)) {
-                    return number_format($number, 0, ',', '.') . "{$suffix}";
-                }
-            }
+<?php
+if (!function_exists('currency_format')) {
+    function currency_format($number, $suffix = ' VND')
+    {
+        if (!empty($number)) {
+            return number_format($number, 0, ',', '.') . "{$suffix}";
         }
-        require "./default/header.php";
-        $number_cart = 0;
-        if (isset($_SESSION['cart'])){
-            $all_cart = $_SESSION['cart'];
-            $number_cart = count($all_cart);
-        }
-    ?>
-    <div class=content> 
-        <div class = "all-course">
-            <h2 style="text-align: center;">Thông tin giỏ hàng của bạn</h2>
-            <br>
-            <div class="course">
-                <table>
+    }
+}
+require "./default/header.php";
+$number_cart = 0;
+if (isset($_SESSION['cart'])) {
+    $all_cart = $_SESSION['cart'];
+    $number_cart = count($all_cart);
+}
+?>
+<div class=content>
+    <div class="all-course">
+        <h2 style="text-align: center;">Thông tin giỏ hàng của bạn</h2>
+        <br>
+        <div class="course">
+            <table>
+                <tr>
+                    <th>Tên</th>
+                    <th>Tác giả</th>
+                    <th>Số bài</th>
+                    <th>Giá</th>
+                    <th>Tương tác</th>
+                    <th>Mua</th>
+                </tr>
+                <?php if ($number_cart > 0) { ?>
+                <?php foreach ($all_cart as $id => $cart) { ?>
                     <tr>
-                        <th>Tên</th>
-                        <th>Tác giả</th>
-                        <th>Số bài</th>
-                        <th>Giá</th>
-                        <th>Tương tác</th>
-                        <th>Mua</th>
+                        <th>
+                            <a href="./view_course.php?id=<?php echo $id ?>">
+                                <?php echo $cart['name']; ?>
+                            </a>
+                        </th>
+                        <th>
+                            <?php echo $cart['author']; ?>
+                        </th>
+                        <th>
+                            <?php echo $cart['lesson']; ?>
+                        </th>
+                        <th>
+                            <?php echo currency_format($cart['price']) ?>
+                        </th>
+                        <th>
+                            <a class="btn-delete" data-id='<?php echo $id ?>'>
+                                Xóa
+                                <i class='bx bx-trash'></i>
+                            </a>
+                        </th>
+                        <th>
+                            <input data-price='<?php echo $cart['price'] ?>' data-id='<?php echo $id ?>'
+                                   class="input-check" type="checkbox" check="false">
+                        </th>
                     </tr>
-                    <?php if ($number_cart > 0) { ?>
-                    <?php foreach($all_cart as $id => $cart) { ?>
-                        <tr>
-                            <th>
-                                <a href="./view_course.php?id=<?php echo $id ?>">
-                                    <?php echo $cart['name']; ?>
-                                </a>
-                            </th>
-                            <th>
-                                <?php echo $cart['author']; ?>
-                            </th>
-                            <th>
-                                <?php echo $cart['lesson']; ?>
-                            </th>
-                            <th>
-                                <?php echo currency_format($cart['price']) ?>
-                            </th>
-                            <th>
-                                <a class = "btn-delete" data-id='<?php echo $id ?>'>
-                                    Xóa
-                                    <i class='bx bx-trash'></i> 
-                                </a>
-                            </th>
-                            <th>
-                                <input data-price='<?php echo $cart['price'] ?>' data-id='<?php echo $id ?>' class = "input-check" type="checkbox" check="false">
-                            </th>
-                        </tr>
-                    <?php } ?>
-                </table>
-                <?php }else{ ?>
-                    </table>
-                    <p class="emty-cart">
-                        Giỏ hàng trống, mua sắm ngay 
-                        <a href="./index.php">Tại đây</a> 
-                    </p>
                 <?php } ?>
-                <p class="total-price">
-                    Tổng Tiền: 0 đ
+            </table>
+            <?php } else { ?>
+                </table>
+                <p class="emty-cart">
+                    Giỏ hàng trống, mua sắm ngay
+                    <a href="./index.php">Tại đây</a>
                 </p>
-            </div>
+            <?php } ?>
+            <p class="total-price">
+                Tổng Tiền: 0 đ
+            </p>
         </div>
     </div>
-    <div class="tab-right">
-        <div class="in4">
-            <div class="my-infor">
-                <h3>
-                    <i class='bx bx-credit-card'></i>
-                    Thông tin thanh toán
-                </h3>
-                <br>
-                <?php if (isset($_SESSION['id']) == true) { ?>
+</div>
+<div class="tab-right">
+    <div class="in4">
+        <div class="my-infor">
+            <h3>
+                <i class='bx bx-credit-card'></i>
+                Thông tin thanh toán
+            </h3>
+            <br>
+            <?php if (isset($_SESSION['id']) == true) { ?>
                 <p>
                     Tên : <?php echo $_SESSION['name'] ?>
                 </p>
                 <br>
-                <p id = "stk">
-                    Số tài khoản: 
+                <p id="stk">
+                    Số tài khoản:
                 </p>
                 <br>
                 <p>
@@ -114,28 +117,28 @@ if (isset($_SESSION['lever'])){session_destroy();}
                     <button id="buy-course" style="display: none">btn an</button>
                 </form>
             <?php } else { ?>
-                Chưa có thông tin 
+                Chưa có thông tin. <br> Bạn phải <a href="login_and_register.php">đăng nhập</a>
                 <p id="my-money" data-currency="0"></p>
             <?php } ?>
-            </div>
-            <div class="my-infor">
-                <h3>
-                    <i class='bx bx-cart'></i>
-                    Thông tin về mặt hàng
-                </h3>
-                <br>
-                <p id="currency-pay">
-                    Số Tiền phải trả: 0
-                </p>
-                <p id="number-course">
-                    Số lượng bài học bạn mua: 0
-                </p>
-            </div>             
-            <lable id="listen-buy-course" class = "buy">Mua hàng ngay</lable>        
         </div>
+        <div class="my-infor">
+            <h3>
+                <i class='bx bx-cart'></i>
+                Thông tin về mặt hàng
+            </h3>
+            <br>
+            <p id="currency-pay">
+                Số Tiền phải trả: 0
+            </p>
+            <p id="number-course">
+                Số lượng bài học bạn mua: 0
+            </p>
+        </div>
+        <lable id="listen-buy-course" class="buy">Mua hàng ngay</lable>
     </div>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="./script/my_cart.js"></script>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="./script/my_cart.js"></script>
 </body>
 </html>
--- script/my_cart.js
+++ script/my_cart.js
@@ -1,26 +1,26 @@
 let total_price = 0;
 let course = [];
 
-function convetArrayToString(a){
+function convetArrayToString(a) {
     var ArrayString = "";
-    for (var i=0; i<a.length; i++){
+    for (var i = 0; i < a.length; i++) {
         ArrayString = ArrayString + a[i]
-        if(i<a.length-1){
+        if (i < a.length - 1) {
             ArrayString = ArrayString + ","
         }
     }
     return ArrayString;
 }
 
-const totalPrice = () =>{
-    let x = total_price.toLocaleString('vi', {style : 'currency', currency : 'VND'});
+const totalPrice = () => {
+    let x = total_price.toLocaleString('vi', {style: 'currency', currency: 'VND'});
 
-    $('.total-price').html("Tổng Tiền "+ x +"");
-    $('#currency-pay').html("Số Tiền phải trả: "+x+"");
-    
+    $('.total-price').html("Tổng Tiền " + x + "");
+    $('#currency-pay').html("Số Tiền phải trả: " + x + "");
+
     // x = money.toLocaleString('vi', {style : 'currency', currency : 'VND'});  
 
-    $('#number-course').html("Số lượng bài học bạn mua: "+ course.length +"")
+    $('#number-course').html("Số lượng bài học bạn mua: " + course.length + "")
 
     $('#mat-hang-se-mua').attr('value', convetArrayToString(course));
 }
@@ -28,20 +28,20 @@ const totalPrice = () =>{
 $(document).ready(function () {
     $('#stk').html("Số tài khoản: " + Math.floor(Math.random() * 100000) + "" + Math.floor(Math.random() * 100000));
 
-    $('.input-check').change(function(){
-        if ($(this).attr('check') == 'false'){
-            $(this).attr('check','true')
+    $('.input-check').change(function () {
+        if ($(this).attr('check') == 'false') {
+            $(this).attr('check', 'true')
             let price = $(this).data('price');
             let id = $(this).data('id')
             total_price += parseInt(price);
-    
+
             course.push(id)
-        }else if ($(this).attr('check') == 'true'){
-            $(this).attr('check','false')
+        } else if ($(this).attr('check') == 'true') {
+            $(this).attr('check', 'false')
             let price = $(this).data('price');
             let id = $(this).data('id')
             total_price -= parseInt(price);
-    
+
             var index = course.indexOf(id);
             if (index !== -1) {
                 course.splice(index, 1);
@@ -49,8 +49,8 @@ $(document).ready(function () {
         }
         totalPrice()
     })
-    
-    $('.btn-delete').click(function(){
+
+    $('.btn-delete').click(function () {
         let id = $(this).data('id')
         $.ajax({
             type: "POST",
@@ -58,16 +58,16 @@ $(document).ready(function () {
             data: {id},
             // dataType: "dataType",
             success: function (response) {
-                
+
             }
         });
 
         // Khi xóa mặt hàng đã chọn cần giảm tiền mua của sản phẩm xóa
-        if ($(this).parent().parent().find(".input-check").attr('check')=="true"){
+        if ($(this).parent().parent().find(".input-check").attr('check') == "true") {
             let price = $(this).parent().parent().find(".input-check").data('price');
             let id = $(this).parent().parent().find(".input-check").data('id')
             total_price -= parseInt(price);
-    
+
             var index = course.indexOf(id);
             if (index !== -1) {
                 course.splice(index, 1);
@@ -76,23 +76,28 @@ $(document).ready(function () {
         totalPrice()
         $(this).parent().parent().remove()
     })
-    
-    $("#btn-logout").click(function() {
+
+    $("#btn-logout").click(function () {
         $.ajax({
             type: "GET",
             url: "./processing/logout.php",
             // data: {id},
             // dataType: "dataType",
-            success: function (response) {    
+            success: function (response) {
             }
         });
         $('.user-login').html('<a class="user-a" href="./login_and_register.php">Đăng nhập</a>');
         $('.user-login').addClass('user');
-        $('.user').removeClass('user-login');     
+        $('.user').removeClass('user-login');
     })
 
     $('#listen-buy-course').click(function () {
-        document.getElementById('buy-course').click();
+        try {
+            document.getElementById('buy-course').click();
+        } catch (e) {
+            window.location.href="login_and_register.php";
+        }
+
     })
 });
 
