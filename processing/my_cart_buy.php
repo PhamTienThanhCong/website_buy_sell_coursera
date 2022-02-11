8013ff6a0c7bb492f9ced3a4c6616bb944397b9b
congphamtienthanh <congphamtienthanh@gmail.com>
2022-02-11 11:13:58 +0700

    fix lỗi mua hàng

--- processing/my_cart_buy.php
+++ processing/my_cart_buy.php
@@ -10,26 +10,41 @@ $course = htmlspecialchars($_POST["course"]);
 $id_course = explode(',', $course);
 
 foreach ($id_course as $id){
-    // Tiền bạc
+
     $sql = "SELECT
                 course.*
             FROM
                 course
             WHERE
                 course.id_course = '$id'";
-    
+
     $money = mysqli_query($connection, $sql);
     $money = mysqli_fetch_array($money);
 
     if(isset($money['price'])){
-        $money_course = $money['price'];   
-        $sql = "INSERT INTO `oder` (`id_user`, `id_course`, `price_buy`) VALUE ('$id_user','$id','$money_course')";
-        mysqli_query($connection, $sql);
+
+        $check = "SELECT
+                        `id_order`
+                    FROM
+                        `oder`
+                    WHERE
+                        (id_user = '$id_user') and (id_course = '$id')";
+
+        $check = mysqli_query($connection, $check);
+        $check = mysqli_fetch_array($check);
+
+        if (isset($check['id_order'])){
+            echo "đã mua rồi";
+        }else{
+            $money_course = $money['price'];   
+            $sql = "INSERT INTO `oder` (`id_user`, `id_course`, `price_buy`) VALUE ('$id_user','$id','$money_course')";
+            mysqli_query($connection, $sql);
+
+            $sql = "INSERT INTO `view_history` (`id_user`, `id_course`, `view`) VALUE ('$id_user','$id','1')";
+            mysqli_query($connection, $sql);
+            echo "mua thành công";
+        }
         unset($_SESSION['cart'][$id]);
-
-        $sql = "INSERT INTO `view_history` (`id_user`, `id_course`, `view`) VALUE ('$id_user','$id','1')";
-        mysqli_query($connection, $sql);
-
     }  
 }
 
