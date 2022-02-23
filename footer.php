9ae05abcec728c91b595810c633ada19329246de
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-02-23 22:51:48 +0700

    fix some bugs

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-<script type="text/javascript" src="./script/all.js"
+<script type="text/javascript" src="./script/all.js"></script>
