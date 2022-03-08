8ef0809ea5bb502d9f4ed7238d6d6bdb0a3db60c
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-03-08 22:31:44 +0700

    fix lại đổi mật khẩu admin

--- admin/manage/my_account.php
+++ admin/manage/my_account.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../default/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
--- admin/manage/processing/my_account_change_password.php
+++ admin/manage/processing/my_account_change_password.php
@@ -5,7 +5,7 @@ $id_user = htmlspecialchars($_SESSION['id']);
 $password = htmlspecialchars($_POST['password']);
 $new_password = htmlspecialchars($_POST['new_password']);
 
-require "../public/connect_sql.php";
+require "../../../public/connect_sql.php";
 
 $sql = "SELECT * FROM `admin` WHERE (`id_admin` = '$id_user') AND (`password` = '$password')";
 $check_user = mysqli_query($connection, $sql);
@@ -23,7 +23,7 @@ else{
             SET
                 `password` = '$new_password'
             WHERE 
-                (`id_user` = '$id_user')";
+                (`id_admin` = '$id_user')";
     mysqli_query($connection, $sql);
     echo 1;
 }
