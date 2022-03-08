0fa40ce6625db4e95d7d1f53954ced7a771d2557
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-03-08 21:55:43 +0700

    processing change pass

---
+++ admin/manage/processing/my_account_change_password.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require "../../check_admin/check_admin_2.php";
+$id_user = htmlspecialchars($_SESSION['id']);
+$password = htmlspecialchars($_POST['password']);
+$new_password = htmlspecialchars($_POST['new_password']);
+
+require "../public/connect_sql.php";
+
+$sql = "SELECT * FROM `admin` WHERE (`id_admin` = '$id_user') AND (`password` = '$password')";
+$check_user = mysqli_query($connection, $sql);
+$check_user = mysqli_fetch_array($check_user);
+
+if ( isset($check_user['id_admin']) == false){
+    echo 0 ;
+}elseif ($password==$new_password){
+    echo 2 ;
+} else if(!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$#",$new_password)){
+    echo "4";}
+else{
+    $sql = "UPDATE
+                `admin`
+            SET
+                `password` = '$new_password'
+            WHERE 
+                (`id_user` = '$id_user')";
+    mysqli_query($connection, $sql);
+    echo 1;
+}
+mysqli_close($connection);
