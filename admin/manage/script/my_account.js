0882a583c60302da183ad83a524aef6edfc5e978
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-03-08 22:11:43 +0700

    thêm phần sửa pass

--- admin/manage/script/my_account.js
+++ admin/manage/script/my_account.js
@@ -16,3 +16,39 @@ var loadFile = function(event) {
         URL.revokeObjectURL(output.src) // free memory
     }
 };
+$('#change-danger').click(function () {
+    document.getElementById('my-password').style.display = 'inline-block';
+    $('#change-danger').css('display', 'none');
+})
+$('#my-password').submit(function () {
+    event.preventDefault();
+
+    if ($('#new-password').val() == $('#confirm-password').val()) {
+        $.ajax({
+            type: "POST",
+            url: "./processing/my_account_change_password.php",
+            dataType: "html",
+            data: $(this).serializeArray(),
+        })
+            .done(function (response) {
+                if (response == 0) {
+                    // alert('Mật khẩu không đúng');
+                    toastr["error"]("Mật khẩu không đúng", "Lỗi");
+                } else if (response == 2) {
+                    toastr["error"]("Bạn vừa nhập mật khẩu cũ", "Lỗi");
+                }else if (response == 4) {
+                    toastr["error"]("Mật khẩu không đủ mạnh", "Lỗi");
+                } else if (response == 1) {
+                    toastr["success"]("Thay đổi mật khẩu thành công", "Thành công");
+                    $('#old-password').val('');
+                    $('#new-password').val('');
+                    $('#confirm-password').val('');
+                    document.getElementById('my-password').style.display = 'none';
+                    $('#change-danger').css('display', 'inline-block');
+                }
+            })
+    } else {
+        toastr["error"]("Mật khẩu xác nhận ko khớp", "Lỗi");
+    }
+
+})
